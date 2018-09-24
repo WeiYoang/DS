f495917a0978a0a9963d6da25cc66498bb31cb5a
杨威 <958700433@qq.com>
2018-09-24 18:39:41 +0800

    Initial Commit

---
+++ DS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS
+//
+//  Created by WeiYoang on 9/24/18.
+//  Copyright © 2018 WeiYoang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
